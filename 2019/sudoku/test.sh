ceb375205c3c3ab4930b962d6c75bd75e1b9f2bf
Michal <zylowskimichal@gmail.com>
2019-04-08 01:15:11 +0200

    Added missing test for sudoku
    Signed-off-by: Michal <zylowskimichal@gmail.com>

--- 2019/sudoku/test.sh
+++ 2019/sudoku/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-expected=(1 0 2 1 1 2 2 1)
+expected=(1 0 2 1 1 2 2 1 1)
 passes=0
 
-for i in 0 1 2 3 4 5 6 7
+for i in 0 1 2 3 4 5 6 7 8
 do
    cp sudoku_tests/${i}.txt sudoku.txt
    java -jar dist/sudoku.jar
